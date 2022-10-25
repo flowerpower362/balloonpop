2f44b2ad71156316ffc836c62560a214a3bda950
flowerpower362 <annalisamiller362@gmail.com>
2022-10-25 12:58:17 -0600

    remaining changes to complete balloon pop

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let startButton = document.getElementById("start-button");
-let inflateButton = document.getElementById("inflate-button");
+//BUTTONS
+
+//#region GAME LOGIC AND DATA
 
 //DATA
 let clickCount = 0;
@@ -9,13 +10,18 @@ let inflationRate = 20;
 let maxsize = 300;
 let highestPopCount = 0;
 let currentPopCount = 0;
-let gameLength = 5000;
+let gameLength = 10000;
 let clockId = 0;
 let timeRemaining = 0;
+let currentPlayer = {};
+let currentColor = "red";
+let possibleColors = ["red", "green", "blue", "purple", "pink"];
 
 function startGame() {
-  startButton.setAttribute("disabled", "true");
-  inflateButton.removeAttribute("disabled");
+  document.getElementById("game-controls").classList.remove("hidden");
+  document.getElementById("main-controls").classList.add("hidden");
+  document.getElementById("scoreboard").classList.add("hidden");
+
   startClock();
   setTimeout(stopGame, gameLength);
 }
@@ -40,14 +46,30 @@ function inflate() {
   clickCount++;
   height += inflationRate;
   width += inflationRate;
+  checkBalloonPop();
+  draw();
+}
 
+function checkBalloonPop() {
   if (height >= maxsize) {
     console.log("Pop the balloon");
+    let balloonElement = document.getElementById("balloon");
+    balloonElement.classList.remove(currentColor);
+    getRandomColor();
+    balloonElement.classList.add(currentColor);
+
+    //@ts-ignore
+    document.getElementById("pop-sound").play();
+
     currentPopCount++;
     height = 0;
     width = 0;
   }
-  draw();
+}
+
+function getRandomColor() {
+  let i = Math.floor(Math.random() * possibleColors.length);
+  currentColor = possibleColors[i];
 }
 
 function draw() {
@@ -55,31 +77,102 @@ function draw() {
   let clickCountElem = document.getElementById("click-count");
   let popCountElem = document.getElementById("pop-count");
   let highpopCountElem = document.getElementById("high-pop-count");
+  let playerNameElem = document.getElementById("player-name");
 
   balloonElement.style.height = height + "px";
   balloonElement.style.width = width + "px";
 
   clickCountElem.innerText = clickCount.toString();
   popCountElem.innerText = currentPopCount.toString();
-  highpopCountElem.innerText = highestPopCount.toString();
+  highpopCountElem.innerText = currentPlayer.topScore.toString();
+
+  playerNameElem.innerText = currentPlayer.name;
 }
 
 function stopGame() {
   console.log("the game is over");
 
-  inflateButton.setAttribute("disabled", "true");
-  startButton.removeAttribute("disabled");
+  document.getElementById("main-controls").classList.remove("hidden");
+  document.getElementById("scoreboard").classList.remove("hidden");
+
+  document.getElementById("game-controls").classList.add("hidden");
 
   clickCount = 0;
   height = 120;
   width = 100;
 
-  if (currentPopCount > highestPopCount) {
-    highestPopCount = currentPopCount;
+  if (currentPopCount > currentPlayer.topScore) {
+    currentPlayer.topScore = currentPopCount;
+    savePlayers();
   }
 
   currentPopCount = 0;
 
   stopClock();
   draw();
+  drawScoreboard();
 }
+
+//#endregion
+
+let players = [];
+loadPlayers();
+
+function setPlayer(event) {
+  event.preventDefault();
+  let form = event.target;
+
+  let playerName = form.playerName.value;
+
+  currentPlayer = players.find((player) => player.name == playerName);
+
+  if (!currentPlayer) {
+    currentPlayer = { name: playerName, topScore: 0 };
+    players.push(currentPlayer);
+    savePlayers();
+  }
+
+  form.reset();
+  document.getElementById("game").classList.remove("hidden");
+  form.classList.add("hidden");
+  draw();
+  drawScoreboard();
+}
+
+function changePlayer() {
+  document.getElementById("player-form").classList.remove("hidden");
+  document.getElementById("game").classList.add("hidden");
+}
+
+function savePlayers() {
+  window.localStorage.setItem("players", JSON.stringify(players));
+}
+
+function loadPlayers() {
+  let playersData = JSON.parse(window.localStorage.getItem("players"));
+  if (playersData) {
+    players = playersData;
+  }
+}
+
+function drawScoreboard() {
+  let template = "";
+
+  players.sort((p1, p2) => p2.topScore - p1.topScore);
+
+  players.forEach((player) => {
+    template += `
+  <div class="d-flex space-between">
+  <span>
+    <i class="fa-solid fa-user"></i>
+    ${player.name}
+  </span>
+  <span>score: ${player.topScore}</span>
+</div> 
+`;
+  });
+
+  document.getElementById("players").innerHTML = template;
+}
+
+drawScoreboard();
